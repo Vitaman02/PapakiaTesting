9e4c9057612184eb4f538538efde35fb4afb1823
Filip Ioannidis <filip1253@gmail.com>
2022-06-04 20:55:22 +0300

    #2 Check if object is visible

--- Assets/DuckMovement.cs
+++ Assets/DuckMovement.cs
@@ -5,25 +5,15 @@ using UnityEngine;
 public class DuckMovement : MonoBehaviour {
 
     public CharacterController controller;
-    float horizontalMove = 0f;
-
     Vector3 staticMovement = new Vector3(0.1f, 0f, 0f);
 
-    // Start is called before the first frame update
-    void Start() {
-        
-    }
-
-    // Update is called once per frame
-    void Update() {
-        // Input.GetAxisRaw("Horizontal");
-        // Debug.Log(Input.GetAxisRaw("Horizontal"));
-        // horizontalMove = Input.GetAxisRaw("Horizontal");
-    }
-
     void FixedUpdate() {
-
+        if (DuckMovement.On)
         controller.Move(staticMovement);
 
+    }
+
+    void OnBecameInvisible() {
+        enabled = false;
     }
 }
